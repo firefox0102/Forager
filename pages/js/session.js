9caa03719a131739c3acd5df272b241ea68080f4
Andrew Pfundstein <drew@myskiprofile.com>
2014-03-24 22:45:50 -0400

    changes

--- pages/js/session.js
+++ pages/js/session.js
@@ -2,19 +2,22 @@ var Session = {
     userId: 1,
     userName: "apfundst",
     name: "Drew Pfundstein",
-    setInfo: function (result) {
+    setInfo: function (result) {//takes a json object, intended to come from server
     	this.name = result.name;
     	this.id = result.id;
     	//console.log(result);
         
     },
     login: function(usn, pass){
-    	
+    	console.log(usn);
+    	console.log(pass);
     	$.post("login_request.php",{un: usn, password: pass})
 
     	.done(function(result) {
     		Session.userName = usn;
     		console.log(result);
+    		Session.setInfo(result);
+    		console.log(Session);
     		
   			//window.location = "http://www.yoururl.com";
 		})
