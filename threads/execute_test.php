a2edb3359170293d59e745bcaed25fb3fe25548f
tkellehe <mitchell.kelleher@gmail.com>
2014-03-27 12:52:53 -0400

    drew
    hope this helps

--- threads/execute_test.php
+++ threads/execute_test.php
@@ -1,3 +1,9 @@
 <?php
+include_once ('../echo_functions.php');
+include_once ('../extra_functions.php');
+include_once ('../database_array_functions.php');
+include_once ('../search_and_test_functions.php');
+include_once ('loop_thread.php');
+
 $t = new LOOPSCANNER("");
 ?>
