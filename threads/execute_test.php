c158347bea97393f6bb633c6d17a26a53a6af530
tkellehe <mitchell.kelleher@gmail.com>
2014-03-28 04:41:12 -0400

    execute_test.php
    -Can now actually start a search just on spsu.edu though
    !-Can place more by just pushing another array onto $ds.

--- threads/execute_test.php
+++ threads/execute_test.php
@@ -4,5 +4,7 @@ include_once ('../server/search_and_test_functions.php');
 include_once ('loop_thread.php');
 include_once ('../alg_queries.php');
 
-$t = new LOOPSCANNER("");
+$ds = new DS();
+$ds[] = array('source'=>"http://spsu.edu/", 'link'=>"");
+$t = new LOOPSCANNER($ds, 1, true);
 ?>
