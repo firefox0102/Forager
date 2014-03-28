07f5be941aca8f9cf5b1164624fa3425b4cd054b
jcathcart1 <jcathcart1@gmail.com>
2014-03-28 04:28:50 -0400

    SQL madness

--- new_scan_table_create.php
+++ new_scan_table_create.php
@@ -1,12 +1,12 @@
 <?php
-
+session_start();
 $con = mysqli_connect("localhost","root","forageme","forager");
 if (mysqli_connect_errno()){
   echo "Failed to connect to server.....can you be cool just once! Just once be cool!: " . mysqli_connect_error();
   exit;
 }
 
-$report_name = $_POST["report_name"];
+$scan_name = $_POST["scan_name"];
 $started_by  = $_POST["started_by"];
 $start_time  = date('H:i:s');
 //DON'T NEED MAX FOR PHASE 3
@@ -14,7 +14,7 @@ $start_time  = date('H:i:s');
 
 $scan_running_sql ="
  		SELECT *
- 		FROM `report`
+ 		FROM `scan`
  		WHERE is_running = 1;
 ";
 $result = mysqli_store_result($con);
@@ -26,11 +26,13 @@ if(is_null($row) ){
 
 //IF max is not set then insert null...
 $new_report_sql ="
-		INSERT INTO `report`(`report_name`,`started_by`,`start_time`,`is_running`)
+		INSERT INTO `scan`(`report_name`,`started_by`,`start_time`,`is_running`)
 		VALUES('$report_name','$started_by','$start_time','1')
 "; 
 $result = mysqli_query($con,$new_report_sql);
 $last_report_id = mysqli_insert_id($con);
+$_SESSION['current_id'] = $last_report_id;
+
 // Creates a new url table for the scan
 $new_url_table_name      = "url_".$last_report_id;
 $new_link_rel_table_name = "link_rel_".$last_report_id;
@@ -38,7 +40,6 @@ $new_link_rel_table_name = "link_rel_".$last_report_id;
 $new_url_table_sql ="
 		CREATE TABLE IF NOT EXISTS `'$new_url_table_name'` (
   		`url_id` int(11) NOT NULL AUTO_INCREMENT,
-  		`report_id` int(11) NOT NULL,
   		`link` varchar(1000) NOT NULL,
   		`source` varchar(1000) NOT NULL,
   		`type` varchar(1000) NOT NULL,
@@ -49,7 +50,6 @@ $new_url_table_sql ="
 		CREATE TABLE IF NOT EXISTS `'$new_link_rel_table_name'` (
   		`url_id` int(11) NOT NULL,
   		`dest_id` int(11) NOT NULL,
-  		`report_id` int(11) NOT NULL
 		) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 ";
 $result = mysqli_query($con,$new_url_table_sql);
