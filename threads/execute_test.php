a2222053465c5981e4d578881518f7ff67ca6c14
tkellehe <mitchell.kelleher@gmail.com>
2014-03-28 04:37:26 -0400

    execute_test.php --Runs single thread
    -Now can use SQL commands

--- threads/execute_test.php
+++ threads/execute_test.php
@@ -2,6 +2,7 @@
 include_once ('../server/extra_functions.php');
 include_once ('../server/search_and_test_functions.php');
 include_once ('loop_thread.php');
+include_once ('../alg_queries.php');
 
 $t = new LOOPSCANNER("");
 ?>
